6feb6242fc2afc13f61497ae5d3497f045005b1e
Saša Barišić <sbarisic@vub.hr>
2024-09-13 20:01:41 +0200

    Added project files

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace GreenCarp {
+	[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin {
+		internal static new ManualLogSource Logger;
+
+		private void Awake() {
+			// Plugin startup logic
+			Logger = base.Logger;
+			Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+		}
+	}
+}

a25aa0bbef6e7746aebdc69581b7006a8298be62
Saša Barišić <sbarisic@vub.hr>
2024-09-14 03:52:44 +0200

    Cleanup

--- src/HUDTextChat.cs
+++ src/HUDTextChat.cs
@@ -24,27 +24,6 @@ namespace GreenCarp {
 			}
 
 			return true;
-
-			/*if (ChatMsg.StartsWith("/save")) {
-				if (P2PSession.Instance != null && P2PSession.Instance.AmIMaster()) {
-					Console.WriteLine("Saving game!");
-					SaveGame.Save();
-					return false;
-				}
-
-				// For each player
-				foreach (ReplicatedLogicalPlayer ply in ReplicatedLogicalPlayer.s_AllLogicalPlayers) {
-					if (ply.GetP2PPeer().IsMaster()) {
-
-						ply.RequestSave();
-
-					}
-				}
-
-
-			}
-
-			return true;*/
 		}
 	}
 
@@ -63,37 +42,9 @@ namespace GreenCarp {
 			ChatCommands.OnChatCommand(DisplayName, Msg, (CmdMsg) => {
 				__instance.StoreMessage(CmdMsg, null, Color.yellow);
 				SendTextChatMessage(net_msg.m_Connection.m_Peer, CmdMsg);
-				//P2PSession.Instance.SendTextChatMessage(CmdMsg);
 			});
 
 			return false;
-
-			/*if (Msg.StartsWith("/")) {
-				string Cmd = Msg.Substring(1);
-				string[] Tokens = Cmd.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-				if (Tokens[0] == "save") {
-					AnswerPlayers(__instance, playerComponent, "Saving game", false);
-					//playerComponent.RequestSave();
-					SaveGame.Save();
-				} else {
-					AnswerPlayers(__instance, playerComponent, string.Format("Unknown command: {0}", Tokens[0]), true);
-				}
-
-			} else
-				__instance.StoreMessage(Msg, DisplayName, new Color?(playerComponent ? playerComponent.GetPlayerColor() : __instance.m_NormalColor));
-
-			return false;*/
-		}
-
-		static void AnswerPlayers(HUDTextChatHistory Instance, ReplicatedLogicalPlayer Ply, string Msg, bool IsError) {
-			Color Clr = Color.green;
-
-			if (IsError)
-				Clr = Color.red;
-
-			Instance.StoreMessage(Msg, "CARP", Clr);
-			//P2PSession.Instance.SendTextChatMessage(Msg);
 		}
 
 		static void SendTextChatMessage(P2PPeer Peer, string message) {
